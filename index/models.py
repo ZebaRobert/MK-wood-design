981a08a72086c2c659e32e81e2d80862a39b154f
ZebaRobert <zeba.robert1@gmail.com>
2024-03-19 19:39:55 +0100

    Create About model

--- index/models.py
+++ index/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class About(models.Model):
+    title = models.CharField(max_lenght=200, unique=True)
+    backgorund_image = CloudinaryField(
+        'image',
+        default='bg_placeholder'
+    )
+    content = models.TextField()
+
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"{self.title}"
+    
+    
