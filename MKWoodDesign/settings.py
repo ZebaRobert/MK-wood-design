34d0903d00eab244fa8ff42e4718d6766ed3b6cf
ZebaRobert <zeba.robert1@gmail.com>
2024-06-03 20:08:16 +0200

    Add email configuration

--- MKWoodDesign/settings.py
+++ MKWoodDesign/settings.py
@@ -151,4 +151,13 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+CONTACT_EMAIL = 'rzeba96@gmail.com'
+
 django_heroku.settings(locals())
--- env.py
+++ env.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault(
     "CLOUDINARY_URL", "cloudinary://783723727299137:MoVCBZJ_znZFVULUlPjTFKGNVWg@dhnjqbynw"
 )
+os.environ['EMAIL_HOST_USER'] = 'zeba.robert1@gmail.com'
+os.environ['EMAIL_HOST_PASSWORD'] = 'Robert13zeba'
