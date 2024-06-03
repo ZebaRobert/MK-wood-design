080b573d5ddeb9acbf8ed24372fe65a224a889ba
ZebaRobert <zeba.robert1@gmail.com>
2024-06-03 19:57:59 +0200

    Add gallery template

--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def gallery(request):
-    return HttpResponse("Hello, world. You're at the Gallery.")
+    return render(request, "gallery/gallery.html")
