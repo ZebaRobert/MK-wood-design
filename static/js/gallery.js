26a296235e0d38cc062e2813080e7893c8583c08
ZebaRobert <zeba.robert1@gmail.com>
2024-06-06 19:55:02 +0200

    Add event listeners to gallery.js

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -1,11 +1,22 @@
 let slideIndex = 0;
 showSlides();
 
+/*  */
 function showSlides() {
     let slides = document.querySelectorAll(".slide");
     slides.forEach((slide, index) => {
         slide.style.display = index === slideIndex ? "block" : "none";
     });
 }
+
+document.querySelector(".prev").addEventListener("click", () => {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlides();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlides();
+});
 
 
