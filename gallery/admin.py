0226eecb6d126aa1a0c9048bed1904538451e65d
ZebaRobert <zeba.robert1@gmail.com>
2024-06-06 18:34:40 +0200

    Add gallery views

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Reviews
+from .models import Reviews , GalleryImage
 
 # Register your models here.
 
 admin.site.register(Reviews)
+admin.site.register(GalleryImage)
--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from .models import GalleryImage, Comments
+import random
 
-
-def gallery_reviews(request):
-    return render(request, "gallery/gallery.html")
+def gallery_view(request):
+    images = GalleryImage.objects.all()
+    reviews = list(Comments.objects.all())
+    random.shuffle(reviews)
+    reviews = reviews[:5]  # Take 5 random reviews
+    return render(request, 'gallery/gallery.html', {'images': images, 'reviews': reviews})
