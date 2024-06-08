099df377ced414d05dcaa21d56177982a231c908
ZebaRobert <zeba.robert1@gmail.com>
2024-06-08 17:35:24 +0200

    Code cleanup

--- UserDetails/urls.py
+++ UserDetails/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import profilePage, login_view , register_view , logout_view
+from .views import login_view, register_view, logout_view, profilePage
 
 urlpatterns = [
     path('login/', login_view, name='login'),
-    path('profile/', profilePage, name="profile"),
     path('register/', register_view, name='register'),
-    path('logout/', logout_view, name="logout"),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profilePage, name='profile'),
 ]
--- UserDetails/views.py
+++ UserDetails/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
-    if request.messages == 'POST':
+    if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
@@ -16,7 +16,7 @@ def login_view(request):
                 return redirect('profile')
     else:
         form = AuthenticationForm()
-    return render(request, 'userdetails/login.html', {'form', form})
+    return render(request, 'userdetails/login.html', {'form': form})
 
 def register_view(request):
     if request.method == 'POST':
@@ -31,7 +31,7 @@ def register_view(request):
                 return redirect('profile')
     else:
         form = UserCreationForm()
-    return render(request, 'authentication/register.html', {'form': form})
+    return render(request, 'userdetails/register.html', {'form': form})
 
 def logout_view(request):
     logout(request)
