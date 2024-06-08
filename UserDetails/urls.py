8abe23c6013b42f61f6196ec85765b842d19405a
ZebaRobert <zeba.robert1@gmail.com>
2024-06-08 16:09:32 +0200

    Edit userdetails  urls.py, create login.html

--- UserDetails/urls.py
+++ UserDetails/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import profilePage
+from .views import profilePage, login_view
 
 urlpatterns = [
+    path('login/', login_view, name='login'),
     path("", profilePage, name="profile"),
 ]
