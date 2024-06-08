87e3625baa75f39697aa837c3de6f15ce8356c47
ZebaRobert <zeba.robert1@gmail.com>
2024-06-08 18:44:40 +0200

    Edit userdetails/urls.py

--- UserDetails/urls.py
+++ UserDetails/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import login_view, register_view, logout_view, profilePage
+from .views import login_view, register_view, logout_view, profilePage, edit_review, delete_review
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('logout/', logout_view, name='logout'),
     path('profile/', profilePage, name='profile'),
+    path('review/edit/<int:review_id>/', edit_review, name='edit_review'),
+    path('review/delete/<int:review_id>/', delete_review, name='delete_review'),
 ]
