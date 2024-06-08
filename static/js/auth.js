fcbcc582a2f4bb71fb0e4aa352ca39f53eb5429f
ZebaRobert <zeba.robert1@gmail.com>
2024-06-08 16:26:31 +0200

    Edit auth.js for register.html

--- static/js/auth.js
+++ static/js/auth.js
@@ -1,17 +1,26 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
+    const showRegister = document.getElementById('show-register');
     const showLogin = document.getElementById('show-login');
     const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
 
     // Set the initial visibility of the forms
     loginForm.style.display = 'block';
     registerForm.style.display = 'none';
 
+    if (showRegister) {
+        showRegister.addEventListener('click', function(event) {
+            event.preventDefault();
+            loginForm.style.display = 'none';
+            registerForm.style.display = 'block';
+        });
+    }
 
-    if (showLogin){
-        showLogin.addEventListener('click', function(event){
+    if (showLogin) {
+        showLogin.addEventListener('click', function(event) {
             event.preventDefault();
             registerForm.style.display = 'none';
             loginForm.style.display = 'block';
-        })
+        });
     }
-})
+});
