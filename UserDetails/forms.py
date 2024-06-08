f751dce15f97d48b3dec7ca4da93fdeb72afa224
ZebaRobert <zeba.robert1@gmail.com>
2024-06-08 18:32:43 +0200

    create form for review editing

--- UserDetails/forms.py
+++ UserDetails/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import CustomUser
+from gallery.models import Reviews
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'first_name', 'last_name', 'date_of_birth', 'phone_number', 'avatar']
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = ['content', 'rating']
