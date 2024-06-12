a0418f44e7595807f6607a1cabd2fd5a9e6c01f8
ZebaRobert <zeba.robert1@gmail.com>
2024-06-12 06:01:51 +0200

    Ready for deployment

--- env.py
+++ env.py
@@ -5,6 +5,6 @@ os.environ['EMAIL_HOST_USER'] = 'zeba.robert1@gmail.com'
 os.environ['EMAIL_HOST_PASSWORD'] = 'xfgt fvnn nxfp yudp'
 os.environ['CLOUDINARY_URL'] = "cloudinary://783723727299137:MoVCBZJ_znZFVULUlPjTFKGNVWg@dhnjqbynw"
 os.environ['SECRET_KEY'] = 'django-insecure-8ww3@)9eg5dghw48k8(n3$(ub&rk0@9lnj7i5c+6mtj!2+)&o_'
-os.environ['CLOUDINARY_CLOUD_NAME'] = "MKWoodDesign"
+os.environ['CLOUDINARY_CLOUD_NAME'] = "dhnjqbynw"
 os.environ['CLOUDINARY_API_KEY'] = "783723727299137"
 os.environ['CLOUDINARY_API_SECRET'] = "MoVCBZJ_znZFVULUlPjTFKGNVWg"
