2de05f9eecc3f72453eacd6613ed459f7eee30f9
ZebaRobert <zeba.robert1@gmail.com>
2024-06-12 03:46:20 +0200

    Fix deployment issuee with cloudinary

--- MKWoodDesign/settings.py
+++ MKWoodDesign/settings.py
@@ -106,9 +106,9 @@ DATABASES = {
 # Cloudinary 
 
 cloudinary.config(
-    cloud_name=os.getenv('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.getenv('CLOUDINARY_API_KEY'),
-    api_secret=os.getenv('CLOUDINARY_API_SECRET'),
+    cloud_name=os.getenv('MKWoodDesign'),
+    api_key=os.getenv('783723727299137'),
+    api_secret=os.getenv('MoVCBZJ_znZFVULUlPjTFKGNVWg'),
     cloudinary_url=os.environ.get('CLOUDINARY_URL')
 )
 
