5fb604c13879eec80d16fb3fe4b56af2d454e789
ZebaRobert <zeba.robert1@gmail.com>
2024-06-12 04:31:38 +0200

    Update settings.py

--- MKWoodDesign/settings.py
+++ MKWoodDesign/settings.py
@@ -107,8 +107,8 @@ DATABASES = {
 
 cloudinary.config(
     cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    api_key=os.environ('CLOUDINARY_API_KEY'),
-    api_secret=os.environ('CLOUDINARY_API_SECRET'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
     cloudinary_url=os.environ.get('CLOUDINARY_URL')
 )
 
