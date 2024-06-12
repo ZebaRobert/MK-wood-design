35daf308c2afb990fb27bde061aeb9d410477f38
ZebaRobert <zeba.robert1@gmail.com>
2024-06-12 04:01:05 +0200

    Fix deployment issue

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os 
 
-os.environ["DATABASE_URL"] = "postgres://u7bi49cqnin385:p9921da270f59b2b61006bb374c341f4d56af92cea8903797c8c66dec99ac3141@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/ddemdih1nopv3f"
+os.environ["DATABASE_URL"] = "postgres://u16lfftmg9cf9l:p8e58f3505db52fec1e1cd96936125b685ebde415ba82d36f189e51154ad8dddd@c9tiftt16dc3eo.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com:5432/d2ss10vcilob6l"
 os.environ['EMAIL_HOST_USER'] = 'zeba.robert1@gmail.com'
 os.environ['EMAIL_HOST_PASSWORD'] = 'xfgt fvnn nxfp yudp'
 os.environ.setdefault(
